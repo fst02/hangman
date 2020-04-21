47fd507ead1dcd06bca0549dfac487c2df12cbb0
bh-fst02 <bh.fst02@gmail.com>
2020-04-21 11:50:14 +0200

    display placeholders

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const words = ['wonderful', 'impossible', 'beautiful', 'adventurous', 'absurd', 'nice', 'book'];
 
-console.log(words);
+let randomWord = words[Math.floor(Math.random() * words.length)];
+
+let placeholders = '';
+for (let i = 0; i < randomWord.length; i++) {
+    placeholders += '_ ';
+}
+document.getElementById('placeholders').innerHTML = placeholders;
