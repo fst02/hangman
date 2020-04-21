054c8a77e1136d91555c0ce7522e70b217115136
bh-fst02 <bh.fst02@gmail.com>
2020-04-21 18:23:26 +0200

    guess matching letters

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+function render() {
+    document.getElementById('placeholders').innerHTML = placeholders.join(' ');
+}
 const words = ['wonderful', 'impossible', 'beautiful', 'adventurous', 'absurd', 'nice', 'book'];
 
 let randomWord = words[Math.floor(Math.random() * words.length)];
 
-let placeholders = '';
+let placeholders = [];
 for (let i = 0; i < randomWord.length; i++) {
-    placeholders += '_ ';
+    placeholders.push('_');
 }
-document.getElementById('placeholders').innerHTML = placeholders;
+render();
 
 let letters = [];
 document.addEventListener('keydown', event => {
     let letter = event.key;
     letters.push(letter);
     document.getElementById('letters').innerHTML = letters.join();
-});
+
+    let guess = randomWord.includes(letter);
+    if (guess) {
+        let letterIndex = randomWord.indexOf(letter);
+        placeholders[letterIndex] = letter;
+        render();
+    }
+})
