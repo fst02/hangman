52eedd10b1e5d8a8c29300ae37e55b7dc1a7ac2c
bh-fst02 <bh.fst02@gmail.com>
2020-04-22 11:54:40 +0200

    #FST2-9 image change for lives count

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 function render() {
     document.getElementById('placeholders').innerHTML = placeholders.join(' ');
 }
+
+function renderImage() {
+    let gallowSrc = 'images/gallow' + lives + '.png';
+    document.getElementById('gallowTree').src = gallowSrc;
+    /*switch (lives) {
+        case 5:
+            document.getElementById('gallowTree').src = 'images/gallow5.png';
+            break;
+        case 4:
+            document.getElementById('gallowTree').src = 'images/gallow4.png';
+            break;
+        case 3:
+            document.getElementById('gallowTree').src = 'images/gallow3.png';
+            break;
+        case 2:
+            document.getElementById('gallowTree').src = 'images/gallow2.png';
+            break;
+        case 1:
+            document.getElementById('gallowTree').src = 'images/gallow1.png';
+            break;
+        case 0:
+            document.getElementById('gallowTree').src = 'images/gallow0.png';
+            break;     
+    }*/
+}
 const words = ['wonderful', 'impossible', 'beautiful', 'adventurous', 'absurd', 'nice', 'book'];
 
-
 let randomWord = words[Math.floor(Math.random() * words.length)];
+document.getElementById('randomWord').innerHTML = randomWord;
 
 let placeholders = [];
 for (let i = 0; i < randomWord.length; i++) {
@@ -12,6 +37,7 @@ for (let i = 0; i < randomWord.length; i++) {
 }
 render();
 
+let lives = 6;
 let letters = [];
 document.addEventListener('keydown', event => {
     let letter = event.key;
@@ -24,13 +50,17 @@ document.addEventListener('keydown', event => {
         while (letterIndex != -1) {
             placeholders[letterIndex] = letter;
             letterIndex = randomWord.indexOf(letter, letterIndex + 1);
-        }
-
-        /*for (let i = 0; i < randomWord.length; i++) {
+        } 
+        /* Alternative of while
+        for (let i = 0; i < randomWord.length; i++) {
             if(randomWord[i] == letter) {
                 placeholders[i] = letter;
             }
         }*/
+
         render();
+    } else {
+        lives -= 1;
+        renderImage(); 
     }
 })
